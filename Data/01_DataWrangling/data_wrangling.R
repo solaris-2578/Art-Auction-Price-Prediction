da9f7d1a8a58b0d0b2393229aec36892e0381bcb
solaris-2578 <coco0210@163.com>
2020-11-08 15:30:31 +0800

    Clean data

--- Data/01_DataWrangling/data_wrangling.R
+++ Data/01_DataWrangling/data_wrangling.R
@@ -11,55 +11,113 @@ daS <-
   read.csv(
     "HK_Sothebys_2016-2020.csv",
     header = T,
-    na.strings = c("", "Unavailable")
+    na.strings = c("", "Unavailable", "[not communicated]")
   )
-dim(daS) # Raw: 5180   17
-head(daS)
-str(daS)
 
-# Filter rows & cols
-daS <- select (daS, -c(edition, foundry)) # 5180   15
-daS <- filter(daS, category == "Paintings") # 2922   15
+# Filter rows
+daS <- filter(daS, category == "Paintings") %>%
+  droplevels()
 
-# Split size to width, length
+# Split
 daS <- daS %>%
   extract(size_cm,
           c("height", "width"),
           "([\\d.]+)[^\\d.]+([\\d.]+)",
           convert = TRUE)
 
-# 
-levels(daS$signed)
-levels(daS$Medium)
+# daS <- daS %>%
+#   separate(Medium,
+#            c("A", "B"),
+#            "/") %>%
+#   mutate(across(c("A", "B"), as.factor))
 
-daS$sales_dollar <- as.numeric(daS$sales_dollar)
+# Drop cols
+daS <-
+  select (daS,-c(edition, foundry, online_dummy, category)) # 2922   13
 
-daS$size_cm[1]
-length(daS$size_cm)
-table(daS$category)
+# Rename & Convert
+as.numeric.factor <- function(x) {
+  as.numeric(levels(x))[x]
+}
+daS <- daS %>%
+  rename(
+    sales_price = sales_dollar,
+    sold_dummy = dummy_sold,
+    low_estimate = low_estimate_dollar,
+    high_estimate = high_estimate_dollar,
+    signiture = signed,
+    medium = Medium
+  )
+
+daS$sales_price <- as.numeric(as.character(daS$sales_price))
+daS$sold_dummy <- as.numeric(as.character(daS$sold_dummy))
+daS$low_estimate <- as.numeric(as.character(daS$low_estimate))
+daS$high_estimate <- as.numeric(as.character(daS$high_estimate))
+
+daS$auction_location <- "Sotheby_HK"
+
+# Display
+dim(daS) # 2922   14
+head(daS)
+str(daS)
+
+levels(daS$signiture)
+levels(daS$medium)
 
 #################### Christie's #############################
 daC <-
   read.csv(
     "HK_Christies_2016-2020.csv",
     header = T,
-    na.strings = c("", "Unavailable")
+    na.strings = c("", "Unavailable", "n/a")
   )
-dim(daC) # Raw: 7067   14
-head(daC)
 
-# Drop useless cols
-daC <- select (daC, -c(Edition, Foundry)) # 7067   12
-daC <- filter(daC, Medium_Clean == "Painting") #2508   12
+# Filter rows
+daC <- filter(daC, Medium_Clean == "Painting")  %>%
+  droplevels()
 
-# Split size to width, length, and height
-# sum(is.na(daC$Medium_Clean))
-daS %>%
-  separate(size_cm, c("height", "width", "depth"), "x")
+# Split size to width, length
+daC <- daC %>%
+  separate(Size, c(NA, "size_cm"), sep = "\\s+\\s+") %>%
+  extract(size_cm,
+          c("height", "width"),
+          "([\\d.]+)[^\\d.]+([\\d.]+)",
+          convert = TRUE)
 
-# Add sold_dummy col
+# Add/Drop cols
 daC$sold_dummy <- as.numeric(!is.na(daC$Sales_Price_Dollar))
+daC$auction_location <- "Christie_HK"
+daC <- select (daC,-c(Edition, Foundry, Medium_Clean))
 
-#
-head(daC$Size)
-df %>% tidyr::extract(Size, "B")
+# Reorder & Rename & Convert
+daC <- daC %>%
+  relocate(sold_dummy, .after = Sales_Price_Dollar)
+daC <- daC %>%
+  rename_at(vars(colnames(daC)), ~ colnames(daS))
+
+daC$sales_price <- as.numeric(as.character(daC$sales_price))
+daC$sold_dummy <- as.numeric(as.character(daC$sold_dummy))
+daC$low_estimate <- as.numeric(as.character(daC$low_estimate))
+daC$high_estimate <- as.numeric(as.character(daC$high_estimate))
+
+# Display
+dim(daC) # Raw: 2508   14
+head(daC)
+str(daC)
+
+#################### Merge #############################
+mydata <- bind_rows(daS, daC)
+mydata$auction_location <- factor(
+  mydata$auction_location,
+  levels = c("Sotheby_HK", "Christie_HK"),
+  labels = c(0, 1)
+)
+mydata <- mydata %>%
+  mutate(surface = height * width) %>%
+  relocate(surface, .after = width)
+
+dim(mydata)
+tail(mydata)
+str(mydata)
+
+write.csv(mydata ,"Auction_HK_2016-2020.csv")
