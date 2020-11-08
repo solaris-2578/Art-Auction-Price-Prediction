d853aa2b599d7317d742fe6847068ea46287a557
solaris-2578 <coco0210@163.com>
2020-11-09 01:04:00 +0800

    Wrangle data

--- eda.R
+++ eda.R
@@ -7,10 +7,12 @@ library(ggplot2)
 
 mydata <- read.csv('./Data/02_CleanData/Auction_HK_2016-2020.csv', header = T)
 
+dim(mydata)
 head(mydata)
 str(mydata)
 summary(mydata)
 
+sum(mydata$sold_dummy == 0)/nrow(mydata)
 plot(mydata$auction_date, mydata$sales_price, ylim = c(0,2e+07)) # give up outliers
 
 
